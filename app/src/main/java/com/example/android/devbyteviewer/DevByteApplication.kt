0417ad932937714be87d2708010cecd51767ee11
pkonrad1229 <pkonrad1229@gmail.com>
2021-11-29 12:00:07 +0100

    Step 8 Completed

--- app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
+++ app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
@@ -18,20 +18,48 @@
 package com.example.android.devbyteviewer
 
 import android.app.Application
+import android.os.Build
+import androidx.work.*
+import com.example.android.devbyteviewer.work.RefreshDataWorker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 /**
  * Override application to setup background work via WorkManager
  */
 class DevByteApplication : Application() {
 
-    // TODO (01) Create CoroutineScope variable applicationScope, using Dispatchers.Default.
+    val applicationScope = CoroutineScope(Dispatchers.Default)
 
-    // TODO (02) Create a delayedInit() function that calls setupRecurringWork() in
-    // the coroutine you defined above.
 
-    // TODO (04) Create a setupRecurringWork() function and use a Builder to define a
-    // repeatingRequest variable to handle scheduling work.
+    private fun delayedInit() = applicationScope.launch {
+        setupRecurringWork()
+    }
+
+    private fun setupRecurringWork() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresBatteryNotLow(true)
+            .setRequiresCharging(true)
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    setRequiresDeviceIdle(true)
+                }
+            }.build()
+
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
+            .setConstraints(constraints)
+            .build()
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+            RefreshDataWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest
+        )
+
+    }
 
     // TODO (05) In setupRecurringWork(), get an instance of WorkManager and
     // launch call enqueuPeriodicWork() to schedule the work.
@@ -50,6 +78,6 @@ class DevByteApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
-        // TODO (03) Call delayedInit().
+        delayedInit()
     }
 }
